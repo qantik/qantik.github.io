3d802cdbc0513b56231f5a2e1cce19a88313bf69
Andrea Caforio <andrea.caforio@epfl.ch>
2017-12-07 14:04:42 +0100

    MathJax

--- js/action.js
+++ js/action.js
@@ -4,4 +4,24 @@ G.addNode(2, {count: 8});
 G.addNode(3, {count: 15});
 G.addEdgesFrom([[1,2],[2,3]]);
 
-jsnx.draw(G, {element:'#demo-canvas'});
+//function draw(n) {
+//    if (n % 2 == 0)
+//        jsnx.draw(G, {element:'#demo-canvas', nodeStyle:{fill: 'red'}});
+//    else
+//        jsnx.draw(G, {element:'#demo-canvas', nodeStyle:{fill: 'blue'}});
+//}
+//
+//var counter = 0;
+//window.onclick = () => {
+//    draw(counter++);
+//}
+
+jsnx.draw(G,  {element:'#demo-canvas', width: 500, height: 500, nodeStyle:{fill: 'red'}});
+
+console.log(MathJax);
+//MathJax.Hub.Config({styles: {
+//        ".MathJax": {
+//            color: "#000000"
+//        }
+//    }
+//    });
